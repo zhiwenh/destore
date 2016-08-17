a4fc7e0b845e7007315dd0012390e992b6856d28
Shray bansal <bansalshray@gmail.com>
2016-08-17 14:40:31 -0700

    finished masterlist

--- contracts/Receiver.sol
+++ contracts/Receiver.sol
@@ -12,7 +12,7 @@ contract Receiver {
 	function Receiver (uint availStorage){
 		owner = msg.sender
 		masterList = '0x9813498237498273487'
-		masterList.call(bytes4(sha3("addReceiver( uint256)")), availStorage);
+		masterList.call(bytes4(sha3("addReceiver(uint256)")), availStorage);
 	}
 
 	//returns storage
@@ -20,16 +20,25 @@ contract Receiver {
 	function retrieveStorage() constant returns (string[]){
 		return hashArray;
 	}
-
+ 
 	//when checked folder and something was deleted or when deliberately deleted file
 	function delete(string hash, uint filesize){
 		masterList.call(bytes4(sha3("deleteHash(uint256)")), filesize);
 		delete hashArray[hash];
 	}
 
+	function addToHashList(string hashIPFS){
+		hashArray[hashArray.length++] = hashIPFS;
+	}
+
 	//function called every 5 minutes to update storage value in object
 	function updateStorage(uint storage){
 		availStorage = storage
+	}
+
+	function recReassign(string[] deletedHashes, uint256[] storage){
+		//update hashArray AND availStorage
+		masterList.call(bytes4(sha3("reassign(string[], uint256[])")), deletedHashes, storage);
 	}
 
 	function destroy(uint256[] storage){
