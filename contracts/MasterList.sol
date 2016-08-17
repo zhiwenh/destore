5056bd443ec94d5ec5b8e54c2396e48915dad8c3
Shray bansal <bansalshray@gmail.com>
2016-08-17 15:29:40 -0700

    finished receiver contract

--- contracts/MasterList.sol
+++ contracts/MasterList.sol
@@ -17,19 +17,19 @@ contract MasterList {
 	}
 
 	//constructor
-	function MasterList(){
+	function MasterList() {
 		owner = msg.sender;
 	}
 
-	function addReceiver(uint availStorage){
-		for(var i = 0; i < receivers.length; i++){
+	function addReceiver(uint availStorage) {
+		for(var i = 0; i < receivers.length; i++) {
 			receivers[receivers.length++] = Receiver(msg.sender, availStorage);
 		}
 	}
 
 	//called by sender to find receiver with given filesize
 	//returns receiverAddress
-	function findReceiver(uint filesize) constant returns (address){
+	function findReceiver(uint filesize) constant returns (address) {
 		for(var i = 0; i < receivers.length; i++){
 			if(filesize < receivers[i].availStorage) {
 				//decrease available Storage of receiver
@@ -40,28 +40,29 @@ contract MasterList {
 		}
 	}		
 
-	function assign(uint filesize, string hashIPFS){
+	//receiver call need to be put inside findReceiver???????
+	function assign(uint filesize, string hashIPFS) {
 		address receiver = this.call(bytes4(sha3("findReceiver(uint256)")), filesize);
 		receiver.call(bytes4(sha3("addToHashList(string)")), hashIPFS);
 	}
 
 	//when receiver is destroyed, reassigns all files to other receivers
-	function reassign(string[] hashAddresses, uint256[] storage){
-		address receiver
-		this.call(bytes4(sha3("findReceiver(uint256)")), filesize);
+	function reassign(string[] hashAddresses, uint256[] filesizes){
+		for (var i = 0; i < hashAddresses.length; i++) { 
+			address receiver = this.call(bytes4(sha3("findReceiver(uint256)")), filesizes[i]);
+			receiver.call(bytes4(sha3("addToHashList(string)")), hashIPFS)
+		}
 	}
 
-	function deleteHash(uint storage){
-		recStorage[msg.sender] += storage;
+	function removeReceiver() {		
+		for (var i = 0; i < receivers.length; i++){
+			if(receivers[i].receiverAddress === msg.sender) delete receivers[i];
+		}
 	}
 
 	//to self-destruct SC and return money to owner
 	function destroy() restricted {
 		suicide(owner);
 	}
-
-
-
-
 
 }
