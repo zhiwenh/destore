17d682750342cc3a8a598c6714c03fcb5d587bbc
Shray bansal <bansalshray@gmail.com>
2016-08-22 16:12:46 -0700

    working demo

--- libs/config/rpc.js
+++ libs/config/rpc.js
@@ -5,13 +5,15 @@ const Web3 = require('web3');
 // ex: localhost? port?
 // maybe ipfs configurations as well
 
+//Zhiwen's ip: 192.168.1.41 (Use only while testing - replace host)
+
 const rpcConfig = {
   host: 'localhost',
   port: 8545,
   provider: new Web3.providers.HttpProvider('http://' + 'localhost' + ':' + '8545')
 };
 
-if (rpcConfig.host === 'localhost') {
+if (rpcConfig.host !== 'localhost') {
   const host = rpcConfig.host;
   const port = rpcConfig.port;
   rpcConfig.provider = new Web3.providers.HttpProvider('http://' + host + ':' + port);
