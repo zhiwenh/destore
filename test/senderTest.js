2124ea15d551d96342dc8668ae8cc7ecec41edc3
campallison <allisonhanna@gmail.com>
2016-08-25 16:59:57 -0700

    worked out a testing plan, added a couple lines to packagejson

--- test/senderTest.js
+++ test/senderTest.js
@@ -1,12 +1,7 @@
 contract('MasterList', function (accounts) {
-  it('This test should fail,', function (done){
-    Sender.new()
-    .then(function(sender){
-      sender.owner.call().then(
-        function(owner){
-          assert.equal(msg.sender, owner, 'msg.sender does not match owner');
-        }
-      ).catch(done);
-    });
+  it('should return bytes32', function (done){
+    var sender = Sender.deploy();
+
+    return sender.testSender.call().then()
   });
 });
