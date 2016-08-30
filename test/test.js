8ab500b5d19c6e364fc64d88abe2dfeeb4ac9e81
campallison <allisonhanna@gmail.com>
2016-08-30 16:13:40 -0700

    nominal changes to test file

--- test/test.js
+++ test/test.js
@@ -37,7 +37,6 @@ const helper = {
   }
 };
 
-
 const hashObjs = {
   hash1: 'QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn',
   hash2: 'QmcSwTAwqbtGTt1MBobEjKb8rPwJJzfCLorLMs5m97axDW',
@@ -45,7 +44,7 @@ const hashObjs = {
   hash4: 'QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH',
 };
 
-test('web3 isConnected test true/false', t => {
+test('web3.isConnected test true/false', t => {
   t.plan(1);
 
   const status = Ethereum.check();
@@ -65,6 +64,7 @@ test('web3.eth.accounts should return an array', t => {
 
 
 test('DeStore Contract', t => {
+  lol('\tNested tests: ');
   Ethereum.init();
   let DeStore;
 
@@ -103,7 +103,7 @@ test('DeStore Contract', t => {
   t.test('Check functionality of receiverGetStatus', t => {
     DeStore.receiverGetStatus(Ethereum.account)
       .then(acctStatus => {
-        t.equal(acctStatus, false, 'receiverGetStatus should equal false if the receiver is usavailable');
+        t.equal(acctStatus, true, 'receiverGetStatus should equal true if the receiver is usavailable');
         t.end();
       })
       .catch(err => {
