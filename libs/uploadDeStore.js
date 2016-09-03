d7ce93f7877eababce77a98e222c853fcb34d695
zhiwenhuang <zhiwen555@gmail.com>
2016-09-03 13:27:26 -0700

    Finished hostDestore, uploadDestore, and mountFile

---
+++ libs/uploadDeStore.js
@@ -0,0 +1,63 @@
+'use strict';
+const IPFS = require('./ipfs/ipfs.js');
+const Ethereum = require('./ethereum/ethereum.js');
+const path = require('path');
+const Upload = require('./../models/Upload.js');
+const promisify = require('es6-promisify');
+const nestedHexToAscii = require('./nestedHexToAscii.js');
+
+const config = require('./config/config.js');
+
+
+/*
+  Adds file to IPFS, deploys sender contract with IPFS hash address, then uploads data to the Upload database
+  @ filePath - string - only does a single file in the electron app
+  @ fileName {String} name that user wants file to be callled in contract
+  @ callback - function - returns the doc created from the Upload.db storage
+*/
+
+
+
+/**
+* gets hashAdddress of a file based on either filepath or name from db and uploads to DeStore
+* @fileName {String} - name of file that has been mounted ex 'kb.png'
+* @returns {Array} - the hashes added to the contract
+**/
+
+module.exports = promisify((fileName, callback) => {
+  Upload.db.findOne({fileName: fileName}, (err, doc) => {
+    if (doc === null) {
+      callback(new Error('No Upload document was found of name ' + fileName), null);
+      return;
+    }
+    let hashArr;
+    let sizeArr;
+    // for doc blocs to have existed would have needed to used method to break them up
+    if (doc.blocks.length >= 1) {
+      hashArr = doc.blocks;
+      sizeArr = doc.blockSizes;
+    } else {
+      hashArr = [doc.hashAddress];
+      sizeArr = [doc.fileSize];
+    }
+
+    const splitArr = [];
+    for (let i = 0; i < hashArr.length; i++) {
+      const half1 = hashArr[i].substring(0, 23);
+      const half2 = hashArr[i].substring(23, 46);
+      splitArr.push([half1, half2]);
+    }
+
+    Ethereum.deStore().senderAddFile(splitArr, fileName, 1, sizeArr)
+      .then(tx => {
+        return Ethereum.deStore().senderGetFileHashes(fileName);
+      })
+      .then(hexHashes => {
+        const asciiHashes = nestedHexToAscii(hexHashes);
+        callback(null, asciiHashes);
+      })
+      .catch(err => {
+        callback(err, null);
+      });
+  });
+});
---
+++ test/appTests.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const tape = require('tape');
+const tapes = require('tapes');
+const tapSpec = require('tap-spec');
+const Ethereum = require('../libs/ethereum/ethereum.js');
+const IPFS = require('./../libs/ipfs/ipfs.js');
+const path = require('path');
+
+const DeStoreAddress = require('./../models/DeStoreAddress.js');
+
+const config = require('./../libs/config/config.js');
+
+const mountFile = require('./../libs/mountFile.js');
+
+tape.createStream()
+  .pipe(tapSpec())
+  .pipe(process.stdout);
+
+const test = tapes(tape);
+
+const lol = console.log.bind(console);
+
+const Upload = require('./../models/Upload.js');
+
+const web3 = Ethereum.init();
+
+Upload.reset();
+
+test('Deploying new DeStore contract and adding a sender and receiver', t => {
+  Ethereum.changeAccount(0);
+  const deployOptions = {
+    from: Ethereum.account
+  };
+  Ethereum.deploy('DeStore', [], deployOptions)
+    .then(instance => {
+      console.log(instance.address);
+      config.contracts.deStore = instance.address;
+      t.equal(instance.address.length, 42, 'Contract address should have a length of 42');
+      return Ethereum.deStore().senderAdd();
+    })
+    .then(tx => {
+      t.end();
+    })
+    .catch(err => {
+      t.fail();
+    });
+});
+
+test('Testing mountFile', t => {
+  mountFile(__dirname + '/kb.png', 1000)
+    .then(res => {
+      t.equal(res.hashAddress, 'QmdMpNvXNKz5AswQ16D2vTFspLBbhsBADXENJnQ7tvDZWs', 'Expect hash uploaded to equal');
+      t.end();
+    })
+    .catch(err => {
+      console.error(err);
+      t.fail(err);
+    });
+});
+
+const uploadDeStore = require('./../libs/uploadDeStore.js');
+
+test('Testing uploadDeStore success', t => {
+  uploadDeStore('kb.png')
+    .then(res => {
+      t.equal(res[0], 'QmdMpNvXNKz5AswQ16D2vTFspLBbhsBADXENJnQ7tvDZWs', 'Expect has uploaded to equal has in');
+      t.end();
+    })
+    .catch(err => {
+      console.error(err);
+      t.fail();
+    });
+});
+
+test('Testing uploadDeStore fail with invalid file name', t => {
+  uploadDeStore('does not exist')
+    .then(res => {
+      t.fail();
+    })
+    .catch(err => {
+      t.ok('There was an error');
+      t.end();
+    });
+});
